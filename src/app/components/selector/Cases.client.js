a6c0230830efbfc103a329edc6b973f0202e05a4
StanH123h <1735443634@qq.com>
2024-06-23 21:42:09 +0800

    feat src: added CasePage and UI

---
+++ src/app/components/selector/Cases.client.js
@@ -0,0 +1,29 @@
+'use client'
+import React from 'react';
+import {Divider, List, Typography, Space} from 'antd';
+import {Caseblock} from "@/app/components/caseblock/caseblock";
+import "./Cases.client.scss"
+
+export default function Cases({data}) {
+    console.log(data)
+    return(
+        <div className={"cases"}>
+            <List
+                grid={{
+                    column:2,
+                    gutter:20,
+                }}
+                className={"cases-list"}
+                dataSource={data}
+                renderItem={(item, index) => (
+                    <>
+                        {index % 2 === 0 && index !== 0 && <Divider className={"cases-list-divider"}/>}
+                        <List.Item>
+                            <Caseblock title={item.title} subtitle={item.subtitle} coverURL={item.cover}/>
+                        </List.Item>
+                    </>
+                )}
+            />
+        </div>
+    );
+}
--- src/app/dashboard/case/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import {NavBar} from "@/app/components/navbar";
-
-export default function CasePage(){
-    return(
-        <NavBar location={"intro"}/>
-    )
-}
