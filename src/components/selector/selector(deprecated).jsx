67856c8acb1b99b8125fb29bec17dfa23cd72992
GaryZhang <ztrgary@outlook.com>
2024-06-24 13:29:06 +0800

    feat(public): add arrow right button SVG icon
    Add a new arrow right button SVG icon to the public directory for use in UI components. The SVG file includes the necessary graphics and is prepared for integration into the project.

---
+++ src/components/selector/selector(deprecated).jsx
@@ -0,0 +1,57 @@
+'use client'
+import FilterBar from "@/components/selector/FilterBar.client";
+import Cases from "@/components/selector/Cases.client";
+import {useEffect, useState} from "react";
+import "./selector(deprecated).scss"
+export default function SelectorDeprecated() {
+    const [type, setType] = useState('all');
+    const [ageGroup, setAgeGroup] = useState('all');
+    const [data,setData]=useState()
+    useEffect(
+        ()=>{
+        fetch("http://localhost:8085/case?type=all&ageGroup=all",{
+            method:"GET",
+            headers:{
+                'Content-Type':"application/json"
+            }
+        }).then(res => res.json())
+            .then(data=>{
+                setData(data)
+            })
+        }
+    ,[])
+    useEffect(() => {
+
+        fetch(`http://localhost:8085/case?type=${type}&ageGroup=${ageGroup}`,{
+            method:"GET",
+            headers:{
+                'Content-Type':"application/json"
+            }
+        }).then(res => res.json())
+            .then(data=>{
+                setData(data)
+                console.log(data[0])
+            })
+    }, [type, ageGroup])
+    return (
+        <div className={"selector"}>
+            <FilterBar
+                options={['所有', '学校', '教师']}
+                onChange={(type) => {
+                    setType(type)
+                }}
+                type={"type"}
+            >
+            </FilterBar>
+            <br/>
+            <FilterBar
+                options={['所有', '小学', '初中','高中']}
+                onChange={(ageGroup) => {
+                    setAgeGroup(ageGroup)
+                }}
+                type={"ageGroup"}
+            ></FilterBar>
+            <Cases data={data}></Cases>
+        </div>
+    )
+}
